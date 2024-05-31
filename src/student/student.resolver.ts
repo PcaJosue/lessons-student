664a8aeadf3f6dea7a51301d89ea391c10d76247
Josué Pacheco <josuepacheco@MacBook-Pro-de-Josue.local>
2024-05-31 16:52:35 -0500

    nestjs mongodb graphql

---
+++ src/student/student.resolver.ts
@@ -0,0 +1,26 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { StudentType } from './student.type';
+import { CreateStudentInput } from './create-student.input';
+import { StudentService } from './student.service';
+
+@Resolver(() => StudentType)
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Query(() => [StudentType])
+  async students() {
+    return this.studentService.getStudents();
+  }
+
+  @Query(() => StudentType)
+  async student(@Args('id') id: string) {
+    return this.studentService.getStudent(id);
+  }
+
+  @Mutation(() => StudentType)
+  async createStudent(
+    @Args('createStudentInput') createStudentInput: CreateStudentInput,
+  ) {
+    return this.studentService.createStudent(createStudentInput);
+  }
+}
